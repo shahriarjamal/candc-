dbdc31c8cf4609b23a17ca0e999a151b37063584
shahriarjamal <dabgadget@gmail.com>
2026-03-04 23:03:58 +0600

    add new feture

---
+++ simple_c++/demo.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main(){
+
+  std::cout<<"Hi,shahriar jamal\n ";
+  std::cout<<"Address:Dinajpur \n";
+  std::cout<<+8801322577895;
+  std::cout<<"--------------------------------\n";
+
+
+
+}
